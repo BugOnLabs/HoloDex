402500b5d041119b7eef7cfadd850d46352906ec
Adrien8989 <adrien.perea@adopteunmec.com>
2024-11-22 19:08:17 +0100

    ajout text pour voir le nom du pokemon recu

--- HoloDex/ContentView.swift
+++ HoloDex/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var cardDetails: CardDetailsAPIResponse? = nil
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(cardDetails?.cardDetailsAPIModel?.name ?? "Loading...")
         }
         .onAppear {
             CardDetailsNetworkServiceImpl().fetchCardDetails(cardId: "mcd19-2", select: []) { result in
                 switch result {
                 case .success(let cardDetails):
+                    self.cardDetails = cardDetails
                     print(cardDetails)
                 case .failure(let error):
                     print(error)
