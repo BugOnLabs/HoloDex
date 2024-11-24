1bcd205e92f81d4014975a1267df3fa31062c991
Adrien8989 <adrien.perea@adopteunmec.com>
2024-11-24 11:52:58 +0100

    add cardRatio + url state case

--- HoloDex/ContentView.swift
+++ HoloDex/ContentView.swift
@@ -12,11 +12,33 @@ struct ContentView: View {
     @State var cardDetailsAPIModel: CardDetailsAPIModel? = nil
     
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: cardDetailsAPIModel?.images?.large ?? "")) { image in
-                image.image?.resizable()
+        GeometryReader { geometry in
+            VStack(alignment: .center, spacing: 10) {
+                    let screenWidth = geometry.size.width
+                    let imageHeight = screenWidth * 1.395
+
+                    AsyncImage(url: URL(string: cardDetailsAPIModel?.images?.large ?? "")) { phase in
+                        switch phase {
+                        case .empty:
+                            ProgressView()
+                                .frame(width: screenWidth, height: imageHeight)
+                        case .success(let image):
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: screenWidth, height: imageHeight)
+                        case .failure:
+                            Image(systemName: "exclamationmark.triangle.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: screenWidth, height: imageHeight)
+                                .foregroundColor(.red)
+                        @unknown default:
+                            EmptyView()
+                        }
+                    }
+                Text(cardDetailsAPIModel?.name ?? "Loading...")
             }
-            Text(cardDetailsAPIModel?.name ?? "Loading...")
         }
         .onAppear {
             CardDetailsNetworkServiceImpl().fetchCardDetails(cardId: "mcd19-2", select: ["name", "id", "images"]) { result in
