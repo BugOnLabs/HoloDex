139437ede3b3494547f62e2f2ce79e6dcbfccf78
Adrien8989 <adrien.perea@adopteunmec.com>
2024-11-24 14:52:30 +0100

    ajout fonction protocol NetworkService

--- HoloDex/Infrastructure/Network/Service/CardDetailsNetworkServiceImpl.swift
+++ HoloDex/Infrastructure/Network/Service/CardDetailsNetworkServiceImpl.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class CardDetailsNetworkServiceImpl {
+class CardDetailsNetworkServiceImpl: NetworkService {
     
     let networkService: NetworkService = MoyaNetworkServiceImpl()
     
