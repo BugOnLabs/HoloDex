88fdfbaf8edec91101edd22c30e063235669df34
Adrien8989 <adrien.perea@adopteunmec.com>
2024-12-04 22:45:06 +0100

    refacto

--- HoloDex/Infrastructure/Network/Service/CardDetailsNetworkServiceImpl.swift
+++ HoloDex/Infrastructure/Network/Service/CardDetailsNetworkServiceImpl.swift
@@ -10,10 +10,10 @@ import Moya
 
 class CardDetailsNetworkServiceImpl {
     
-    let provider = MoyaNetworkServiceImpl()
+    let networkService: NetworkService = MoyaNetworkServiceImpl()
     
     func getCardDetail(id: String, select: [String], completion: @escaping (Result<CardDetailsAPIResponse, Error>) -> Void) {
-        provider.request(target: PokemonTcgApi.getCard(id: id, select: select)) { (result: Result<CardDetailsAPIResponse, Error>) in
+        networkService.request(target: PokemonTcgApi.getCard(id: id, select: select)) { (result: Result<CardDetailsAPIResponse, Error>) in
             switch result {
             case .success(let response):
                 completion(.success(response))
