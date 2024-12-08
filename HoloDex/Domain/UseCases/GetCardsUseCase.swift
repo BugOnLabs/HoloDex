7ca928d6ae2ae3ebd358305e4f7c2ed7ad94a86a
Adrien8989 <adrien.perea@adopteunmec.com>
2024-12-08 20:30:12 +0100

    try clean

---
+++ HoloDex/Domain/UseCases/GetCardsUseCase.swift
@@ -0,0 +1,40 @@
+//
+//  GetCardsUseCase.swift
+//  HoloDex
+//
+//  Created by Adrien PEREA on 08/12/2024.
+//
+
+import Foundation
+
+protocol GetCardsUseCase {
+    func getCards(
+        requestValue: GetCardsUseCaseRequestValue,
+        completion: @escaping (Result<CardsAPIResponse, Error>) -> Void
+    )
+}
+
+struct GetCardsUseCaseRequestValue: Equatable {
+    let pageSize: Int
+    let page: Int
+    let select: [String]
+}
+
+final class DefaultGetCardsUseCase: GetCardsUseCase {
+    private let cardsRepository: CardsRepository
+
+    init(
+        cardsRepository: CardsRepository
+    ) {
+        self.cardsRepository = cardsRepository
+    }
+
+    func getCards(requestValue: GetCardsUseCaseRequestValue, completion: @escaping (Result<CardsAPIResponse, any Error>) -> Void) {
+        return cardsRepository.fetchCardsList(pageSize: requestValue.pageSize,
+                                                page: requestValue.page,
+                                                select: requestValue.select,
+                                                completion: { result in
+                                                    completion(result)
+                                                })
+    }
+}
--- HoloDex/Presentation/CardsScene/CardsList/ViewModel/CardsViewModel.swift
+++ HoloDex/Presentation/CardsScene/CardsList/ViewModel/CardsViewModel.swift
@@ -9,11 +9,16 @@ import Foundation
 
 class CardsViewModel: ObservableObject {
     
+    init(cardsUseCase: GetCardsUseCase) {
+        self.cardsUseCase = cardsUseCase
+    }
+    
     @Published var cardDetailsAPIModels: [CardDetailsAPIModel]? = nil
-    let cardsNetworkService = CardsNetworkServiceImpl()
+
+    let cardsUseCase: GetCardsUseCase
     
     func fetchCards(pageSize: Int, page: Int, select: [String]) {
-        cardsNetworkService.getCards(pageSize: pageSize, page: page, select: select){ result in
+        cardsUseCase.getCards(requestValue: GetCardsUseCaseRequestValue(pageSize: pageSize, page: page, select: select)){ result in
             switch result {
             case .success(let cardsApiResponse):
                 self.cardDetailsAPIModels = cardsApiResponse.cardDetailsAPIModels
